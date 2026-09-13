fda6ca20a32b3dc0d8fed937f0977807cd145b89
Peto <ian.schilling566@passmail.com>
2026-09-13 10:50:33 +0200

    path corrected

--- map.js
+++ map.js
@@ -364,7 +364,7 @@
       ring.style.cursor = 'pointer';
     }
 
-    fetch('atlas_sk.geojson')
+    fetch('./atlas_sk.geojson')
       .then((response) => {
         if (!response.ok) throw new Error(`Failed to load GeoJSON: ${response.status}`);
         return response.json();
